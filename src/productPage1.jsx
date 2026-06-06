944bcd1f71765094b625486949ad331998984415
fellstinger <gotsisleon@gmail.com>
2026-06-06 17:43:47 +0200

    fix?

--- src/productPage1.jsx
+++ src/productPage1.jsx
@@ -3,23 +3,15 @@ import Product from "./components/Product";
 import ProductData from "./components/ProductData";
 
 const ProductPage = () => {
-    const [productData, setProductData] = React.useState(null);
+    const productData = ProductData(1);
 
-    React.useEffect(() => {
-        const fetchData = async () => {
-            const data = await ProductData(1);
-            console.log(data);
-            setProductData(data);
-        };
-        fetchData();
-    }, []);
     const id = productData ? productData.id : null;
     if (!productData) {
         return <div>Loading...</div>;
     }
     return (
         id &&
-        <div key={1} className="product">
+        <div key={1}>
             <h1>{productData.title}</h1>
             <h3>Rating: {productData.rating}</h3>
             <p>{productData.description}</p>
